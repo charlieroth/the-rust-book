54ec28b6e7846612d3de5a43c3d0f0b2bb2dbc71
charlieroth <charlieroth4@gmail.com>
2024-09-09 12:46:21 +0200

    Ignore collections/target directory

--- collections/target/debug/deps/collections-da31b02e321f238a.d
+++
@@ -1,5 +0,0 @@
-/home/charlie/github.com/charlieroth/the-rust-book/collections/target/debug/deps/libcollections-da31b02e321f238a.rmeta: src/main.rs
-
-/home/charlie/github.com/charlieroth/the-rust-book/collections/target/debug/deps/collections-da31b02e321f238a.d: src/main.rs
-
-src/main.rs:
