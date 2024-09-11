8e7a6bbd8e67f290b428426401f32cd83f74cfea
charlieroth <charlieroth4@gmail.com>
2024-09-11 15:20:11 +0200

    Remove testing/target

--- testing/target/debug/deps/testing-16c6ef335823495d.d
+++
@@ -1,5 +0,0 @@
-/Users/charlie/github.com/charlieroth/the-rust-book/testing/target/debug/deps/testing-16c6ef335823495d: src/lib.rs
-
-/Users/charlie/github.com/charlieroth/the-rust-book/testing/target/debug/deps/testing-16c6ef335823495d.d: src/lib.rs
-
-src/lib.rs:
--- testing/target/debug/deps/testing-a46e9aab1728be1d.d
+++
@@ -1,5 +0,0 @@
-/Users/charlie/github.com/charlieroth/the-rust-book/testing/target/debug/deps/libtesting-a46e9aab1728be1d.rmeta: src/lib.rs
-
-/Users/charlie/github.com/charlieroth/the-rust-book/testing/target/debug/deps/testing-a46e9aab1728be1d.d: src/lib.rs
-
-src/lib.rs:
