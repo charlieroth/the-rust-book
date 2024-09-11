96eeb0f2040d5bf102669a915e0bcad9036eb06e
charlieroth <charlieroth4@gmail.com>
2024-09-11 15:46:43 +0200

    minigrep: push error handling to main function by returning Result from run function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 struct Config {
     query: String,
@@ -26,11 +26,19 @@ fn main() {
 
     println!("Searching for '{}'", config.query);
     println!("In file {}", config.file_path);
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Problem while running program: {e}");
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.file_path).expect("Failed to read file at path provided");
+// `Box<dyn Error>`` is a trait object, meaning the function will return a type that implements the `Error` trait.
+//
+// Gives the flexibility to return error values that may be different types in different error cases.
+//
+// `dyn` short for  dynamic
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{contents}");
+    Ok(())
 }
