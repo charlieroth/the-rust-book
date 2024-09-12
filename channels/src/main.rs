e649fafc3a7e48a9441aaf114bfe39d5de97e893
charlieroth <charlieroth4@gmail.com>
2024-09-12 21:02:05 +0200

    channels: create multiple producers by cloning transmitter

--- channels/src/main.rs
+++ channels/src/main.rs
@@ -3,6 +3,7 @@ use std::{sync::mpsc, thread, time::Duration};
 fn main() {
     let (tx, rx) = mpsc::channel::<String>();
 
+    let tx1 = tx.clone();
     thread::spawn(move || {
         let msgs = vec![
             String::from("hi"),
@@ -12,8 +13,22 @@ fn main() {
         ];
 
         for msg in msgs {
-            tx.send(msg).expect("Transmitter failed to send message");
+            tx1.send(msg).unwrap();
             thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    thread::spawn(move || {
+        let msgs = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for msg in msgs {
+            tx.send(msg).unwrap();
+            thread::sleep(Duration::from_millis(300));
         }
     });
 
